408f53f18e89bb7956ad9c154ea8ddd7aa44bbac
Sandro Sodré Nunes <86071326+sandrosodrenunes@users.noreply.github.com>
2024-05-19 08:27:26 -0300

    Create SendEmail

--- main.js
+++ main.js
@@ -2,8 +2,20 @@
 URL = 'http://localhost:3000/contatos'
 //=================================================================================================
 // GET - Recupera todos os Contados e adiciona na tabela
-
 let quantContatos = 0;
+
+// teste 
+
+const host = "smtp.elasticemail.com";
+const userNameLogin = "infosocialpuc@gmail.com";
+const password = "C88408DB12F78B7B7ACCC98B41014813C9B0";
+const toEmail = 'infosocialpuc@gmail.com';
+const fromEmail = 'infosocialpuc@gmail.com';
+const subject = "New Email Duvidas";
+let bodyEmail = '';
+let userEmail = '';
+let userName = '';
+
 
 fetch(URL)
     .then(res => res.json())
@@ -35,7 +47,6 @@ fetch(URL)
         //     `;
         //     produtoList.innerHTML = lista_contatos;
         // }
-
     });
 
 // CREATE or UPDATE - PROCEDIMENTO PARA CRIAR OU EDITAR UM CONTATO
@@ -44,15 +55,25 @@ const duvidasForm = document.getElementById('duvidas-form');
 
 duvidasForm.addEventListener('submit', (e) => {
 
+    e.preventDefault();
+
+    let contDuvidas = (quantContatos + 1).toString()
+
     //let id = parseInt($('#id').text());    
 
     // RECUPERA OS DADOS DO CONTADOS
     const contado = JSON.stringify({
-        id: (quantContatos + 1).toString(),
+        id: contDuvidas,
         nome: document.getElementById('nome-id').value,
         email: document.getElementById('email-id').value,
         assunto: document.getElementById('assunto-id').value
     })
+
+    userEmail = document.getElementById('email-id').value;
+    userName = document.getElementById('nome-id').value;
+    bodyEmail = document.getElementById('assunto-id').value;
+    
+    sendEmail();
 
     fetch(URL, {
         method: 'POST',
@@ -64,8 +85,33 @@ duvidasForm.addEventListener('submit', (e) => {
     .then(res => res.json())
     .then(() => location.reload());  
 
+
 });
 
+async function sendEmail() {
+    Email.send({
+        Host : host,
+        Username : userNameLogin,
+        Password : password,
+        To : toEmail,
+        From : fromEmail,
+        Subject : subject,
+        Body : `Olá, sou ${userName}. ${bodyEmail}. Meu Email ${userEmail}`
+    }).then(
+      message => {
+        alert(message);
+        document.getElementById('email-id').value = '';
+        document.getElementById('nome-id').value = '';
+        document.getElementById('assunto-id').value = '';
+    }      
+    );
+}
+
+
+
+
+
+  
 
 
 
