90071f34fcf24c61aee31f6be7568c78c5e8c126
Marco Túlio Candeo <marcotuliocandeo@outlook.com>
2024-06-27 19:48:37 -0300

    fix coordinates

--- src/encontrar-cras.js
+++ src/encontrar-cras.js
@@ -1,93 +1,93 @@
 const cras = [
   {
     "endereco": "Rua Água da Vida, 14 - Independência",
-    "longitude": -43.9502,
-    "latitude": -19.9207
+    "longitude": -43.9498,
+    "latitude": -19.9173
   },
   {
     "endereco": "Rua Frederico Boy Prussiano, 137 - Petrópolis",
-    "longitude": -43.9263,
-    "latitude": -19.9379
+    "longitude": -43.9354,
+    "latitude": -19.9082
   },
   {
     "endereco": "Rua Faisão, 1071 Flávio Marques Lisboa",
-    "longitude": -43.9389,
-    "latitude": -19.8918
+    "longitude": -43.9243,
+    "latitude": -19.8911
   },
   {
     "endereco": "Rua Dona Benta, 145 Vila Fátima",
-    "longitude": -43.9264,
-    "latitude": -19.9335
+    "longitude": -43.9312,
+    "latitude": -19.9163
   },
   {
     "endereco": "Rua Engenheiro Lucas Júlio de Proença, 73 Serra",
-    "longitude": -43.9262,
-    "latitude": -19.9328
+    "longitude": -43.9267,
+    "latitude": -19.9315
   },
   {
     "endereco": "Rua São Tomaz de Aquino, 640 Vila Santa Rita de Cássia",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9348,
+    "latitude": -19.9278
   },
   {
     "endereco": "Centro de Apoio Comunitário Alto Vera Cruz (Rua General Osório esquina com Fernão Dias)",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9201,
+    "latitude": -19.9134
   },
   {
     "endereco": "Rua do Grupo, 12 Granja de Freitas",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9324,
+    "latitude": -19.9229
   },
   {
     "endereco": "Rua Fernão Dias, nº 220, Mariano de Abreu",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9265,
+    "latitude": -19.9352
   },
   {
     "endereco": "Rua Ludgero Felipe Ferreira com Joaquim Teixeira dos Anjos, 42 - Taquaril",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9309,
+    "latitude": -19.9176
   },
   {
     "endereco": "Rua Prof. Geraldo Fontes, 30 - União",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9202,
+    "latitude": -19.9165
   },
   {
     "endereco": "Rua Neblina, 120 Conjunto Paulo VI",
-    "longitude": -43.9261,
-    "latitude": -19.9327
+    "longitude": -43.9338,
+    "latitude": -19.9141
   },
   {
     "endereco": "Rua Cipriano del Favero, 808 - Centro",
-    "longitude": -48.2837,
-    "latitude": -18.9189
+    "longitude": -48.2810,
+    "latitude": -18.9178
   },
   {
     "endereco": "Rua Rio Grande do Sul, 931 - Bairro Brasil",
-    "longitude": -48.2837,
-    "latitude": -18.9189
+    "longitude": -48.2795,
+    "latitude": -18.9150
   },
   {
     "endereco": "Av. Dr. Manoel Tomaz T. de Souza, 758 - Bairro Tocantins",
-    "longitude": -48.2837,
-    "latitude": -18.9189
+    "longitude": -48.2832,
+    "latitude": -18.9144
   },
   {
     "endereco": "Rua Cipriano del Favero, 808 - Bairro Pampulha",
-    "longitude": -48.2837,
-    "latitude": -18.9189
+    "longitude": -48.2729,
+    "latitude": -18.9127
   },
   {
     "endereco": "Rua Cipriano del Favero, 808 - Bairro Presidente Roosevelt",
-    "longitude": -48.2837,
-    "latitude": -18.9189
+    "longitude": -48.2694,
+    "latitude": -18.9119
   },
   {
     "endereco": "Rua Cipriano del Favero, 808 - Bairro São Jorge",
-    "longitude": -48.2837,
-    "latitude": -18.9189
+    "longitude": -48.2641,
+    "latitude": -18.9185
   }
 ]
 
